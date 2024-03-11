db566ceab5ed8a0986c1663f06cd9285a5e6a512
hannaoyo <hannaoyo11@gmail.com>
2024-03-11 19:52:15 +0900

    チャットルーム作成

--- app/controllers/trains_controller.rb
+++ app/controllers/trains_controller.rb
@@ -1,4 +1,31 @@
 class TrainsController < ApplicationController
   def index
+    @trains = Train.order("trainname ASC")
   end
+
+  def new
+    @train = Train.new
+  end
+
+  def create
+    @train = Train.new(train_params)
+    if @train.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    train = Train.find(params[:id])
+    train.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def train_params
+    params.require(:train).permit(:trainname_id)
+  end
+
 end
--- app/models/train.rb
+++ app/models/train.rb
@@ -1,2 +1,9 @@
 class Train < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :trainname, class_name: 'TrainName'
+  has_many :train_users, dependent: :destroy
+  has_many :users, through: :train_users
+  has_many :messages, dependent: :destroy
+  #選択が「---」の時は保存できないようにする
+  validates :trainname_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
